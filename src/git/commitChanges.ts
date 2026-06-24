7f1085ac5bfb401566a738e7b3a4de25cf35e4fa
SaajidAhamed007 <saajidahamed01@gmail.com>
2026-06-24 06:58:33 +0530

    feat:add mock files for github Actions

--- src/git/commitChanges.ts
+++ src/git/commitChanges.ts
@@ -3,17 +3,17 @@ import { execSync } from "child_process";
 export function commitChanges() {
 
   execSync(
-    'git config user.name "ReleasePilot Bot"'
+    'git config user.name "github-actions[bot]"'
   );
 
   execSync(
-    'git config user.email "bot@releasepilot.ai"'
+    'git config user.email "41898282+github-actions[bot]@users.noreply.github.com"'
   );
 
-  execSync("git add .");
+  execSync("git add CHANGELOG.md RELEASE_NOTES.md");
 
   execSync(
-    'git commit -m "docs: update changelog"'
+    'git commit -m "docs: auto generate release notes"'
   );
 
   execSync("git push");
